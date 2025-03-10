bbee18a1c1a194752a8b6923df0bc1afdd3433d0
hemsej018 <hemsej018@naver.com>
2025-03-10 16:29:20 +0900

    Feat(Tea): Tea 클래스 구현

---
+++ src/Tea.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public class Tea extends MenuItem {
+    private String type; // Hot, Iced
+
+    public Tea(String name, double basePrice, String description, List<String> options, String type){
+        super(name, basePrice, description, options);
+        this.type = type;
+    }
+
+    @Override
+    public double getPrice() {
+        return basePrice;
+    }
+
+    @Override
+    public void printInfo() {
+        System.out.printf("%s (%s) | W %1.f | %s\n",name,type,getPrice(),description);
+    }
+}
