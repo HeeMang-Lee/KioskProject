03c32a7b5b7246df5d69ece451295c09dde28b93
hemsej018 <hemsej018@naver.com>
2025-03-12 17:43:00 +0900

    Feat(CartItem):단순히 MenuItem을 저장하는 것이 아니라, 메뉴와 수량을 함께 저장하는 CartItem 클래스 추가

--- src/cart/Cart.java
+++ src/cart/Cart.java
@@ -1,5 +1,6 @@
 package cart;
 
+import menu.*;
 import java.util.ArrayList;
 import java.util.List;
 
--- src/cart/CartItem.java
+++ src/cart/CartItem.java
@@ -1,4 +1,30 @@
 package cart;
 
+import menu.*;
+
 public class CartItem {
+    private MenuItem menuItem;
+    private int quantity;
+
+    public CartItem(MenuItem menuItem, int quantity) {
+        this.menuItem = menuItem;
+        this.quantity = quantity;
+    }
+    // menuItem Getter
+    public MenuItem getMenuItem() {
+        return menuItem;
+    }
+    // quantity Getter
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void increaseQuantity(int amount) {
+        this.quantity += amount;
+    }
+
+    public void printCartItem() {
+        System.out.printf("%s | 수량: %d | 총 가격: W %.1f\n",
+                menuItem.getName(),quantity,menuItem.getPrice()* quantity);
+    }
 }
