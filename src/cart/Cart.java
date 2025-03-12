71f15ccb203c8ad9a0ad98d73ba9349119af5749
hemsej018 <hemsej018@naver.com>
2025-03-12 19:39:09 +0900

    Refactor(Cart): CartItem을 활용하도록 변경

--- src/cart/Cart.java
+++ src/cart/Cart.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cart {
-    private List<MenuItem> items; // 장바구니에 담긴 항목 리스트
+    private List<CartItem> items; // 장바구니에 담긴 항목 리스트
 
     // 생성자
     public Cart() {
@@ -13,15 +13,24 @@ public class Cart {
     }
 
     // 메뉴를 장바구니에 추가하는 메서드
-    public void addToCart(MenuItem item) {
-        items.add(item);
+    public void addToCart(MenuItem item,int quantity) {
+        for(CartItem cartItem : items){
+            if (cartItem.getMenuItem().getName().equals(item.getName())) {
+                cartItem.increaseQuantity(quantity); // 같은 메뉴가 들어오면 수량 증가
+                return;
+            }
+        } items.add(new CartItem(item,quantity)); // 새로운 메뉴는 장바구니에 추가
     }
 
     // 장바구니를 출력하는 메서드
     public void printCart() {
         System.out.println("장바구니 목록: ");
-        for (MenuItem item : items) {
-            item.printInfo();
+        if (items.isEmpty()){
+            System.out.println("장바구니가 비었습니다.");
+            return;
+        }
+        for (CartItem cartItem : items) {
+            cartItem.printCartItem();
         }
     }
 }
